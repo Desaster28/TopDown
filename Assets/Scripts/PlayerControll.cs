1f12a0b07348d0e9ac82b10903216aa7712140aa
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2019-10-09 15:22:31 +0200

    nothing new

--- Assets/Scripts/PlayerControll.cs
+++ Assets/Scripts/PlayerControll.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
+    public float speed;
+    public int health = 5;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
