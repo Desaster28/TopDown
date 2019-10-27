35c8dc9283ca2bd4d5977491323554a8f1e52aee
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2019-10-27 15:57:31 +0100

    added Borders, Adjusted Normalize in movement, Changed the behavior of the border with friction, added lerp to rotation, added some packages to test out later and modified the enemy a little to be better

---
+++ Assets/Border.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Border : MonoBehaviour
+{
+
+
+    public Rigidbody2D playerRB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerRB.velocity = Vector3.zero;
+
+    }
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerRB.velocity = Vector3.zero;
+    }
+    void Update()
+    {
+        
+    }
+}
