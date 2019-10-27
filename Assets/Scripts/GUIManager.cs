2498d958486e5a25d3ae99e362dbc4d84da649c4
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2019-10-27 20:41:31 +0100

    added Timer for Scoreboard(to be implemented)

--- Assets/Scripts/GUIManager.cs
+++ Assets/Scripts/GUIManager.cs
@@ -7,11 +7,17 @@ public class GUIManager : MonoBehaviour
 {
 
     public Text scoreTxt;
+    public Text countTimer;
     public Image[] healthP;
     public Image[] healthM;
+    public TextMesh txt;
     // Start is called before the first frame update
     public void setScore(string score){
         scoreTxt.text = "Score: "+ score;
+    }
+    public void setTime(string time)
+    {
+        countTimer.text = time + "s";
     }
     public void HealthM(){}
     public void HealthP(){}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,22 +8,39 @@ public class GameManager : MonoBehaviour
     private int score;
     public int health;
     private PlayerControll player;
+    private float currTime;
+    private float startTime;
 
 
    // Start is called before the first frame update
     private void Start()
     {
+        startTime = Time.time;
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControll>();
         health = player.health;
         score = 0;
-        MyGUIManager.setScore(score.ToString());    
+        MyGUIManager.setScore(score.ToString());
+        currTime = 0f;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+        float t = Time.time - startTime;
+        string minutes = ((int)t / 60).ToString();
+        string seconds;
+        if (t % 60 < 10) {
+            seconds = 0 + (t % 60).ToString("f1");
+        }
+        else
+        {
+            seconds = (t % 60).ToString("f1");
+        }
+
+        currTime += 1 * Time.deltaTime;
+        MyGUIManager.setTime(minutes + ":" + seconds);
     }
+    public float getTime() { return currTime; }
     public void ScoreUp(int scorePlus){
         score+=scorePlus;
         MyGUIManager.setScore(score.ToString());
