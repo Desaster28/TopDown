ffe9544e3baa64881ba5effc196718210f4f5517
Desaster28 <Desaster28@users.noreply.github.com>
2019-10-27 21:42:27 +0100

    Shoot Enemy

---
+++ Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public int damagePerHit = 0;
+
+    private PlayerControll player;
+
+    Rigidbody2D rb;
+    PlayerControll target;
+    Vector2 moveDirection;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControll>();
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControll>();
+        moveDirection = (target.transform.position - transform.position).normalized * moveSpeed;
+        rb.velocity = new Vector2(moveDirection.x, moveDirection.y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player.damageIntake(damagePerHit);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ShootEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootEnemy : MonoBehaviour
+{
+    private float nextFire;
+    
+    public float fireRate;
+    public GameObject bullet;
+    void Start()
+    {
+        nextFire = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfTimeToFire();
+    }
+
+    void CheckIfTimeToFire()
+    {
+        if(Time.time > nextFire)
+        {
+            Instantiate(bullet, transform.position, Quaternion.identity);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
