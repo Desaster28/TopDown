6f3ae8d0cd07582af18cdf07a9f6e9bb589d40a3
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2019-10-29 22:10:00 +0100

    Added Highscore Template

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+
+
+
+    private void Awake()
+    {
+        entryContainer = transform.Find("highscoreEntryContainer");
+        entryTemplate = transform.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+    }
+
+
+
+
+
+}
